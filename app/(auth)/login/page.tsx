79ddcdd29ed7c30ad26ffaad293c18421c6674bf
hilmy <ysffajarulloh@gmail.com>
2025-12-25 23:28:06 +0700

    fix: login auth

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,18 @@
 import LoginView from '@/components/login/LoginView'
+import { redirect } from 'next/navigation'
+import { isAuthenticated } from '@/lib/auth'
 
 export const metadata = {
   title: 'Login | Member Remora',
   description: 'Mulai perjalanan trading Anda bersama Remora',
 }
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const authenticated = await isAuthenticated()
+  
+  if (authenticated) {
+    redirect('/dashboard')
+  }
+
   return <LoginView />
 }
